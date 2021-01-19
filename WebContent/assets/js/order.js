4557d96be7314666d557371775a0bc07954b16e7
yewon <syewon0128@gmail.com>
2021-01-19 23:47:44 +0900

    basket_order_JS

--- WebContent/assets/js/order.js
+++ WebContent/assets/js/order.js
@@ -9,7 +9,9 @@ $( document ).ready( function() {
         // 체크박스 개별적으로 선택 시 모두 체크인지 확인해서 .check_all에도 자동 선택처리 되도록
         $( '.ab' ).click( function() {
         	if($("#item1").is(":checked") && $("#item2").is(":checked")){
-		  		$( '.check_all' ).prop( 'checked', this.checked );
+		  		$( '.check_all' ).attr("checked", true);
+        	}else{
+        		$( '.check_all' ).attr("checked", false);
         	}
         } );
         
