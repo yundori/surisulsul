d86fc585743a51477b350010eb71d6e74130e61c
Jeong Wooju <jeongwj119@gmail.com>
2021-01-29 15:53:51 +0900

    git 초기화

--- README.md
+++
@@ -1,8 +0,0 @@
-# SuriSS
-수리술술 프로젝트 페이지입니다.
-
-## Hello World
-
-1. item1
-1. item2
-1. item3
--- WebContent/assets/js/juneung.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() { 
-	$(".icons").hover(
-        function() {
-            //console.log("hover 들어옴");
-            $(this).parent().find(".icon_description").toggleClass("text_visible");
-        }
-    );
-})
