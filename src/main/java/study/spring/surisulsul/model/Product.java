415bca6768199f0d4ed03266450a886542d4733d
hyeonsuu <yui3331@naver.com>
2021-02-01 17:21:44 +0900

    .

--- src/main/java/study/spring/surisulsul/model/Product.java
+++ src/main/java/study/spring/surisulsul/model/Product.java
@@ -25,5 +25,27 @@ public class Product {
 
 	// 2) Join 절에 따른 추가 컬럼
 	
+	// 3) 페이지 구현을 위한 static 변수
+			/** LIMIT 절에서 사용할 조회 시작 위치 */
+			private static int offset;
+			
+			/** LIMIT 절에서 사용할 조회할 데이터 수 */
+			private static int listCount;
+			
+			public static int getOffset() {
+				return offset;
+			}
+			
+			public static void setOffset(int offset) {
+				Product.offset = offset;
+			}
+			
+			public static int getListCount() {
+				return listCount;
+			}
+			
+			public static void setListCount(int listCount) {
+				Product.listCount = listCount;
+			}
 	
 }
