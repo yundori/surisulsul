82319628c0df3723d4d042882475b77a37b9b84e
Yang Serim <serymy@gmail.com>
2021-02-02 10:43:38 +0900

    product service update

--- src/main/java/study/spring/surisulsul/service/impl/ProductServiceImpl.java
+++ src/main/java/study/spring/surisulsul/service/impl/ProductServiceImpl.java
@@ -1,7 +1,46 @@
 package study.spring.surisulsul.service.impl;
 
+import java.util.List;
+
+import study.spring.surisulsul.model.Product;
 import study.spring.surisulsul.service.ProductService;
 
 public class ProductServiceImpl implements ProductService{
+
+	@Override
+	public Product getProductItem(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getProductList(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getProductCount(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addProduct(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int editProduct(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteProduct(Product input) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
